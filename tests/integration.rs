2a56a64a0e3e3efa1d99b35adfd65c44d5b3e4f1
Liam Kinne <liam@kinne.io>
2023-07-05 06:46:49 +1000

    cleanup integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,16 +1,17 @@
+use ratelimit::{Limiter, RateLimit, Response};
 use std::time::Duration;
 
-use ratelimit::{Limiter, RateLimit, Response};
+lazy_static::lazy_static! {
+    static ref REDIS: redis::Client = redis::Client::open("redis://127.0.0.1/").unwrap();
+}
 
 #[test]
-fn test_add() {
-    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-
+fn test_success() {
     let ratelimit = RateLimit::builder()
-        .redis(client)
+        .redis(REDIS.clone())
         .limiter(Limiter::FixedWindow {
-            tokens: 10,
-            window: Duration::from_millis(1000),
+            tokens: 5,
+            window: Duration::from_millis(10),
         })
         .build()
         .unwrap();
@@ -18,14 +19,10 @@ fn test_add() {
     let result = ratelimit.limit("test".to_string()).unwrap();
 
     match result {
-        Response::Success {
-            limit,
-            remaining,
-            reset,
-        } => {
+        Response::Success { .. } => {
             assert!(true)
         }
-        Response::Failure { limit, reset } => {
+        Response::Failure { .. } => {
             assert!(false)
         }
     }
